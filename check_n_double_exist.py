5b66956539ad55dc0bc167269655ef7904c51747
shakthi raj <69976520+raj9426@users.noreply.github.com>
2022-10-02 00:13:14 +0530

    hacktoberfest challenge contri

---
+++ check_n_double_exist.py
@@ -0,0 +1,16 @@
+#1346. Check If N and Its Double Exist
+class Solution:
+    def checkIfExist(self, arr) -> bool:
+        
+        map = {}
+        for i in arr:
+            if i*2 in map or i/2 in map:
+                return True
+            map[i]=1
+        return False
+
+
+
+
+s = Solution()
+print(s.checkIfExist([1,2,5,3]))
